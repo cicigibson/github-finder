344507f2cc7b034ad47d280f18b0e93b075261cc
cicigibson <cicicancode@gmail.com>
2021-04-20 13:27:05 -0700

    Utilizing React Router to add pages

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
 	render() {
@@ -7,6 +8,14 @@ export class Navbar extends Component {
 				<h1>
 					<i className={this.props.icon} /> {this.props.title}
 				</h1>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+				</ul>
 			</nav>
 		);
 	}
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class UserItem extends Component {
 	render() {
-		const { login, avatar_url, html_url } = this.props.user;
+		const { login, avatar_url } = this.props.user;
 
 		return (
 			<div className='card text-center'>
@@ -14,9 +15,9 @@ export class UserItem extends Component {
 				/>
 				<h3>{login}</h3>
 				<div>
-					<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					<Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
 						More
-					</a>
+					</Link>
 				</div>
 			</div>
 		);
